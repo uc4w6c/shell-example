30afcdda74494f31084ef61e08cbacc559945470
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-11-16 13:18:25 +0900

    連想配列作成中

--- shell/AssociativeArray.sh
+++ shell/AssociativeArray.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# set
 declare -A musics=(
 	["ultra soul"]="2001"
-	["OCEAN"]="2005"
+	["OCEAN"]="2006" # ああ
 	["BANZAI"]="2004"
 	["RED"]="2015"
-	)
+	["GIFT"]="2008"
+)
 
+musics["ARIGATO"]="2004"
+musics["OCEAN"]="2005"
+unset musics["GIFT"]
+
+# read
+echo "${musics["ultra soul"]}"
+echo "count:${#musics[@]}"
+
+# for
 for music in "${!musics[@]}"; do
     echo "${music}:${musics[${music}]}"
 done
