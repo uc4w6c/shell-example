65e1c317b80ab1795a180b650e211151afd8ef4b
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-11-17 11:34:59 +0900

    連想配列のまとめ完了

--- shell/AssociativeArray.sh
+++ shell/AssociativeArray.sh
@@ -3,7 +3,7 @@
 # set
 declare -A musics=(
 	["ultra soul"]="2001"
-	["OCEAN"]="2006" # ああ
+	["OCEAN"]="2006"
 	["BANZAI"]="2004"
 	["RED"]="2015"
 	["GIFT"]="2008"
@@ -17,8 +17,39 @@ unset musics["GIFT"]
 echo "${musics["ultra soul"]}"
 echo "count:${#musics[@]}"
 
+echo "# -n option"
+if [ -n "${musics["ultra soul"]}" ]; then
+    echo "exist"
+fi
+if [ -n "${musics["GIFT"]}" ]; then
+    echo "not exist"
+fi
+
+echo "# -z option"
+if [ -z "${musics["ultra soul"]}" ]; then
+    echo "exist"
+fi
+if [ -z "${musics["GIFT"]}" ]; then
+    echo "not exist"
+fi
+
 # for
+echo "# for key loop"
+for music in "${!musics[@]}"; do
+    echo "${music}"
+done
+
+echo "# for value loop"
+for music in "${musics[@]}"; do
+    echo "${music}"
+done
+
+echo "# for key/value loop"
 for music in "${!musics[@]}"; do
     echo "${music}:${musics[${music}]}"
 done
-
+# echo "${!musics[*]}"
+# echo "${!musics[@]}"
+# for music in "${musics[*]}"; do
+#     echo "${music}"
+# done
